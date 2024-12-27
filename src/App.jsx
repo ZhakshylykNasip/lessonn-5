4237dadca56a59e4b7c2cb89a157710862f1a570
zhakshylyk <zhakshylyknasipbekov@gmail.com>
2024-12-27 23:39:10 +0600

    create folders and hb

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Happy birthday to you, John</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora animi
+        consequuntur eum soluta quam accusamus sed quidem explicabo vitae esse.
+        Molestiae quam eos facere cumque reprehenderit ex voluptates repellendus
+        incidunt.
+      </p>
+
+      <iframe
+        width="720"
+        height="405"
+        src="https://rutube.ru/play/embed/ab39fc13f13b9c17d964f2c5814f2865/"
+        frameBorder="0"
+        allow="clipboard-write; autoplay"
+        webkitAllowFullScreen
+        mozallowfullscreen
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
